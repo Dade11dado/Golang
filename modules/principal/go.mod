009a0141021d40fc2c6f2b7b954760dbbb091260
Davide Vavassori <davidevavassori@MacBook-Pro-di-Davide.local>
2025-07-13 15:01:08 +0200

    add everything

--- modules/principal/go.mod
+++ modules/principal/go.mod
@@ -1,4 +1,4 @@
-module github.com/Dade11dado/Golang
+module mymodule
 
 go 1.24.5
 
---
+++ modules/principal/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Dade11dado/puppy"
+)
+
+func main() {
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	fmt.Println(s1, s2)
+}
